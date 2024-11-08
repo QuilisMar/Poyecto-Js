ed6e5668bc7ef1c678368bc12e636b56d5ff2b64
Martin30 <mar_riel30@yahoo.com.ar>
2024-11-08 12:02:38 -0300

    Creo la funcion sendEmail para que el backend envie el formulario a jsmail directamente

---
+++ functions/sendEmail.js
@@ -0,0 +1,32 @@
+function sendEmail() {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const fecha = document.getElementById('fecha-cumpleaños').value;
+    const pais = document.getElementById('pais').value;
+
+    fetch("https://criticine.netlify.app//.netlify/functions/sendEmail", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre,
+            apellido,
+            email,
+            fecha,
+            pais
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message === 'Email enviado exitosamente') {
+            mostrarNotificacionEmergente("¡Registro exitoso!", "success");
+        } else {
+            mostrarNotificacionEmergente("Error al enviar el formulario", "error");
+        }
+    })
+    .catch(error => {
+        mostrarNotificacionEmergente("Error al enviar el formulario: " + error.message, "error");
+    });
+}
